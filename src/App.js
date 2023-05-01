fc5450b8cfc57dc2d5c067c939ebeef759a2a909
Vika <stigizimvika@ukr.net>
2023-05-01 14:29:59 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import contents from './contents';
+import {Main_Content} from './main'
 
-export default App;
+
+
+export default function App(){
+    return (
+        <div className='App'>
+            {contents.map(contents => (
+                <Main_Content
+                    key={contents.id}
+                    photo={contents.photo}
+                    name={contents.name}
+                    nickname={contents.nickname}
+                    content={contents.content}
+                    date={contents.date}
+                />
+            ))}
+        </div>
+    )
+}
---
+++ src/contents.js
@@ -0,0 +1,12 @@
+
+const RAY_IMAGE=[
+    {
+        id: "1",
+        name: "Anakin skywalker",
+        photo: "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale",
+        nickname: "@dart_vader",
+        content:"WTF? Who is Ray? Why she is Skywalker? Luke...?",
+        date:"26 квітня",
+    }
+]
+export default RAY_IMAGE;
